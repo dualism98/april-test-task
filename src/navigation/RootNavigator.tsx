4ddda8b1dd91e0059ddcfc3a4be4b0d6bb8b2574
dualism <beachsidebearhi@gmail.com>
2023-11-26 17:23:14 +0300

    Added icons to bottom tabs

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
@@ -5,6 +6,7 @@ import NavigationKeys from './NavigationKeys';
 import PostsStack from './stacks/PostsStack';
 import SettingsScreen from '../screens/SettingsScreen';
 import {RootParamsList} from './types';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const BottomTab = createBottomTabNavigator<RootParamsList>();
 
@@ -13,12 +15,23 @@ const RootNavigator: React.FC = () => {
     <BottomTab.Navigator>
       <BottomTab.Screen
         name={NavigationKeys.PostsStack}
-        options={{tabBarLabel: 'Posts', headerShown: false}}
+        options={{
+          tabBarLabel: 'Posts',
+          tabBarIcon: ({color, size}) => (
+            <Icon name={'format-list-bulleted'} color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
         component={PostsStack}
       />
       <BottomTab.Screen
         name={NavigationKeys.SettingsScreen}
-        options={{tabBarLabel: 'Settings'}}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => (
+            <Icon name={'cog'} color={color} size={size} />
+          ),
+        }}
         component={SettingsScreen}
       />
     </BottomTab.Navigator>
